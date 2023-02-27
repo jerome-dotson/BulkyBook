6c49738f3e89a66d5b52c53c1fe25fd83e97a6e6
jerome-dotson <jerome.lamar.dotson@gmail.com>
2023-02-27 10:18:36 -0500

    Change made to Program.cs

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
